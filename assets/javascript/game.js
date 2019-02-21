1a8b262bd49ecca5999b5cf594e62a27cb7bb3f3
albarrank <albarrankevin97@gmail.com>
2019-02-20 18:32:27 -0800

    jQuery linked and responisve

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,35 @@
-// GLOBAL VARIABLES 
+//  GLOBAL VARIABLES
+var computerNumberChoices = [];
+
+//var randomComputerNubmer = 
+var gemValues = [];
+
+// jQuery
+$(document).ready(function(){
+
+    //GENERATES NUMBERS FOR CLICKS
+    
+        randomComputerNubmer();
+        randomGemValues();
+
+    function randomComputerNubmer() {
+        console.log("hello");
+        for (var i = 19; i <= 120; i++) {
+            computerNumberChoices.push(i);
+        }
+        console.log(computerNumberChoices);
+    }
+    
+    function randomGemValues() {
+        for (var j =1; j <= 12; j++) {
+            gemValues.push(j);
+        }
+        console.log(gemValues);
+    
+    }
+
+});
+
+
+
+
